b6545f23f73aea3ab07433ebadcf61180f12d71c
Lucimar Santos-LS|Soluções <Lucimarcontas@Gmail.com>
2025-06-01 16:22:48 -0300

    Update config.php

--- php/config.php
+++ php/config.php
@@ -3,20 +3,20 @@
 
 // --- Configurações do Banco de Dados ---
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'lu4825');
-define('DB_PASSWORD', 'pti@2018'); // Sua senha real
-define('DB_NAME', 'funilVendas');
-define('DB_TABLE', 'leads_pdf');   // O nome da sua tabela de leads
+define('DB_USERNAME', '');
+define('DB_PASSWORD', ''); // Sua senha real
+define('DB_NAME', '');
+define('DB_TABLE', '');   // O nome da sua tabela de leads
 
 // --- Configurações do PHPMailer (E-mail) ---
 define('SMTP_HOST', 'smtp.gmail.com');
 define('SMTP_PORT', 465); // Porta 465 para ENCRYPTION_SMTPS
 define('SMTP_ENCRYPTION', 'ssl'); // ou 'tls' dependendo do caso, mas 'ssl' para 465
-define('EMAIL_FROM_ADDRESS', 'lucimarcontas@gmail.com');
-define('EMAIL_FROM_NAME', 'Lucimar Santos - Assistente Financeira');
-define('SMTP_USERNAME', 'lucimarcontas@gmail.com'); // Geralmente o mesmo que EMAIL_FROM_ADDRESS
-define('SMTP_PASSWORD', 'psnolddusihrjuhb');       // Sua senha de app do Gmail
-// NOTA: 'psnolddusihrjuhb' é uma senha de aplicativo. NUNCA use sua senha principal do Gmail aqui!
+define('EMAIL_FROM_ADDRESS', '');
+define('EMAIL_FROM_NAME', '');
+define('SMTP_USERNAME', ''); // Geralmente o mesmo que EMAIL_FROM_ADDRESS
+define('SMTP_PASSWORD', '');       // Sua senha de app do Gmail
+
 
 // A parte de conexão com o banco de dados foi removida daqui,
 // pois ela será tratada no arquivo processa_lead.php usando as constantes acima.
